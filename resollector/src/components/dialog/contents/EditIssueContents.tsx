ec9aeef132f1409175ff20f604ebe898ef53a5bc
sikakujira <sikakujira@gmail.com>
2024-06-16 01:48:57 +0900

    コンフリクト解決３

--- resollector/src/components/dialog/contents/EditIssueContents.tsx
+++ resollector/src/components/dialog/contents/EditIssueContents.tsx
@@ -3,18 +3,12 @@ import TextButton from '../../button/TextButton';
 import FilledButton from '../../button/FilledButton';
 import Box from '../../box/Box';
 import TextFieldOutline from '../../textField/TextFieldOutline';
-<<<<<<< HEAD
 import { useState, useEffect, useRef } from 'react';
 import themeColor from '../../../utils/themeColor';
 import TextAreaOutline from '../../textField/TextAreaOutline';
 import  useFetch  from '../../../services/useFetch';
 import getCookie from '../../../services/getCookie';
 
-=======
-import { useState } from 'react';
-import themeColor from '../../../utils/themeColor';
-import TextAreaOutline from '../../textField/TextAreaOutline';
->>>>>>> 9abf34c (フォルダ機能前)
 
 const ContentsPaper = styled.div`
         width: 100%;
@@ -26,19 +20,14 @@ type Props = {
     issueTitle: string,
     issueURL: string,
     issueNote: string,
-<<<<<<< HEAD
     issueId: number,
     folderName: string,
-=======
-    issueId: string
->>>>>>> 9abf34c (フォルダ機能前)
 }
 
 function EditIssueContents(props: Props) {
     const [titleValue, setTitleValue] = useState<string>(props.issueTitle);
     const [URLValue, setURLValue] = useState<string>(props.issueURL);
     const [noteValue, setNoteValue] = useState<string>(props.issueNote);
-<<<<<<< HEAD
     const [folderValue, setFolderValue] = useState<string>(props.folderName);
     const [error, setError] = useState<ErrorMessage>({isError: false, title: "", folder: ""});
     const { onClick, issueTitle, issueURL, issueNote, folderName, issueId } = props;
@@ -101,9 +90,6 @@ function EditIssueContents(props: Props) {
             });
         }
     }, [errorResponse]);
-=======
-
->>>>>>> 9abf34c (フォルダ機能前)
 
     function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>): void {
             setTitleValue(e.target.value);
@@ -117,7 +103,6 @@ function EditIssueContents(props: Props) {
             setNoteValue(e.target.value);
     }
 
-<<<<<<< HEAD
     function handleFolderNameChange(e: React.ChangeEvent<HTMLInputElement>): void {
             setFolderValue(e.target.value);
     }
@@ -132,24 +117,14 @@ function EditIssueContents(props: Props) {
             setURLValue(props.issueURL);
             setNoteValue(props.issueNote);
             setFolderValue(props.folderName);
-=======
-    function handleCancel(): void {
-            setTitleValue(props.issueTitle);
-            setURLValue(props.issueURL);
-            setNoteValue(props.issueNote);
->>>>>>> 9abf34c (フォルダ機能前)
             if(props.onClick){
             props.onClick();
             }
     }
 
     function handleSubmit(): void {
-<<<<<<< HEAD
         sendRequest();
         effectRef.current = true;
-=======
-        //送信処理
->>>>>>> 9abf34c (フォルダ機能前)
     }
 
     return(
@@ -177,11 +152,8 @@ function EditIssueContents(props: Props) {
             type="text"
             labelColorLight={themeColor.light.surfaceContainerHigh}
             labelColorDark={themeColor.dark.surfaceContainerHigh}
-<<<<<<< HEAD
             errorText={error.title}
             isError={error.isError}
-=======
->>>>>>> 9abf34c (フォルダ機能前)
             />
          <TextFieldOutline
             mode={props.mode}
@@ -191,16 +163,11 @@ function EditIssueContents(props: Props) {
             left="10%"
             name="issueurl"
             value={URLValue}
-<<<<<<< HEAD
             placeholder="URL"
-=======
-            placeholder="Issue URL"
->>>>>>> 9abf34c (フォルダ機能前)
             onChange={handleURLChange}
             type="url"
             labelColorLight={themeColor.light.surfaceContainerHigh}
             labelColorDark={themeColor.dark.surfaceContainerHigh}
-<<<<<<< HEAD
             isError={error.isError}
             />
         <TextFieldOutline
@@ -218,21 +185,13 @@ function EditIssueContents(props: Props) {
             labelColorDark={themeColor.dark.surfaceContainerHigh}
             errorText={error.folder}
             isError={error.isError}
-=======
->>>>>>> 9abf34c (フォルダ機能前)
             />
         <TextAreaOutline
             mode={props.mode}
             cols={40}
-<<<<<<< HEAD
             rows={8}
             width="80%"
             top="23rem"
-=======
-            rows={10}
-            width="80%"
-            top="18rem"
->>>>>>> 9abf34c (フォルダ機能前)
             left="10%"
             name="issuenote"
             value={noteValue}
@@ -240,10 +199,7 @@ function EditIssueContents(props: Props) {
             onChange={handleNoteChange}
             labelColorLight={themeColor.light.surfaceContainerHigh}
             labelColorDark={themeColor.dark.surfaceContainerHigh}
-<<<<<<< HEAD
             isError={error.isError}
-=======
->>>>>>> 9abf34c (フォルダ機能前)
             />
         <TextButton
             height="2.5rem"
